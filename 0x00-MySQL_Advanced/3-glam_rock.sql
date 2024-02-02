ab094f4cee214351989ecdbddd12022a0484fe3a
browniwils <browniwils@gmail.com>
2024-02-02 12:16:01 +0000

    Complete project 0x00

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Selecting and modifying data in db
+
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
